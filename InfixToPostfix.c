20c96055f4f7070cff8365d4663a5de89020ccba
mahesh <maheshudas2077@gmail.com>
2025-04-01 01:10:03 +0545

    new codes are added.

--- InfixToPostfix.c
+++ InfixToPostfix.c
@@ -1,136 +1,165 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdbool.h>
 #define MAX 50
 
 char stack[MAX];
 int tos = -1;
 
-bool isempty(){
+bool isempty()
+{
     return tos == -1;
 }
-void push(char ch){
+void push(char ch)
+{
     stack[++tos] = ch;
 }
-char pop(){
-    if(!isempty())
+char pop()
+{
+    if (!isempty())
         return stack[tos--];
     return ' ';
 }
-char peek(){
+char peek()
+{
     return stack[tos];
 }
 
-int precedende(char op){
-    if(op=='!') return 1; //highest precedence
-    if(op=='^' || op=='$') return 2;
-    if(op=='*' || op=='/' || op=='%') return 3;
-    if(op=='+' || op=='-') return 4;
-    else return 5;
+int precedende(char op)
+{
+    if (op == '!')
+        return 1; // highest precedence
+    if (op == '^' || op == '$')
+        return 2;
+    if (op == '*' || op == '/' || op == '%')
+        return 3;
+    if (op == '+' || op == '-')
+        return 4;
+    else
+        return 5;
 }
 
-bool CheckPrecedende(char op1, char op2){
+bool CheckPrecedende(char op1, char op2)
+{
     return precedende(op1) < precedende(op2);
 }
 
-int whatCharacterIs(char ch){
-    if((ch>=65 && ch<=90) || (ch>=97 && ch<=122)) 
+int whatCharacterIs(char ch)
+{
+    if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
         return 1; // if the character is Letter.
 
-    if(ch=='(') return 2;
-    if(ch==')') return 3;
+    if (ch == '(')
+        return 2;
+    if (ch == ')')
+        return 3;
 
-    if(ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='$' || ch=='^' || ch=='%')
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '$' || ch == '^' || ch == '%')
         return 4;
 
     return 0;
 }
 
-void TableRow(char infix[], char postfix[], int i, int j){
-    if(infix[i]=='\0') printf("\n\t...");
-    else printf("\n\t%c",infix[i]);
-
-    printf("\t\t");
-        if(isempty()) printf("...");
-        else 
-            for(int a=0; a<=tos;a++)
-                printf("%c",stack[a]);
-
-    printf("\t\t");
-    if(j==0) printf("...");
+void TableRow(char infix[], char postfix[], int i, int j)
+{
+    if (infix[i] == '\0')
+        printf("\n\t...");
     else
-        for(int a=0; a<j; a++)
-            printf("%c",postfix[a]);
+        printf("\n\t%c", infix[i]);
+
+    printf("\t\t");
+    if (isempty())
+        printf("...");
+    else
+        for (int a = 0; a <= tos; a++)
+            printf("%c", stack[a]);
+
+    printf("\t\t");
+    if (j == 0)
+        printf("...");
+    else
+        for (int a = 0; a < j; a++)
+            printf("%c", postfix[a]);
 }
 
-int main(){
-    char infix[MAX],postfix[MAX];
-    int i=0;
+int main()
+{
+    char infix[MAX], postfix[MAX];
+    int i = 0;
     char ch;
 
-
     printf("Enter the Infix expression:\n");
-    do{
+    do
+    {
         ch = getch();
-        printf("%c",ch);
+        printf("%c", ch);
 
-        if(ch != 13)
+        if (ch != 13)
             infix[i++] = ch;
 
-    }while(ch != 13);
-    infix[i] = '\0'; 
+    } while (ch != 13);
+    infix[i] = '\0';
 
-    //print table
+    // print table
     printf("\n\nProcess Table\n");
     printf("\n\tInput\t\tStack\t\tPostfix\n");
 
     int j = 0;
-    for(i=0; infix[i] != '\0'; i++){     
-        switch(whatCharacterIs(infix[i])){
-            case 1: 
-                postfix[j++] = infix[i];
-                break;
-            case 2:
+    for (i = 0; infix[i] != '\0'; i++)
+    {
+        switch (whatCharacterIs(infix[i]))
+        {
+        case 1:
+            postfix[j++] = infix[i];
+            break;
+        case 2:
+            push(infix[i]);
+            break;
+        case 3:
+            while (peek() != '(' && !isempty())
+            {
+                postfix[j++] = pop();
+            }
+            if (!isempty())
+                pop();
+            break;
+        case 4:
+            if (isempty() || peek() == '(')
                 push(infix[i]);
-                break;
-            case 3:
-                while(peek()!='(' && !isempty()){
+            else
+            {
+
+                if (CheckPrecedende(peek(), infix[i]))
+                {
                     postfix[j++] = pop();
-                }
-                if(!isempty()) pop();
-                break;
-            case 4:
-                if(isempty() || peek()=='(') 
                     push(infix[i]);
-                else{
-                    
-                    if(CheckPrecedende(peek(),infix[i])){
-                        postfix[j++] = pop();
-                        push(infix[i]);
-                    }else{
-                        push(infix[i]);
-                    }
                 }
-                
-                break;
+                else
+                {
+                    push(infix[i]);
+                }
+            }
+
+            break;
         }
 
-        TableRow(infix,postfix,i,j);
+        TableRow(infix, postfix, i, j);
     }
-    while(!isempty()){
-        if(peek()!='(')
+    while (!isempty())
+    {
+        if (peek() != '(')
             postfix[j++] = pop();
         else
             pop();
-        TableRow(infix,postfix,i,j);
+        TableRow(infix, postfix, i, j);
     }
-    TableRow(infix,postfix,i,j);
+    TableRow(infix, postfix, i, j);
 
     postfix[j] = '\0';
 
     printf("\n\nPostfix Expression is :\n");
-    for(i=0; postfix[i]!='\0'; i++)
-        printf("%c",postfix[i]);
+    for (i = 0; postfix[i] != '\0'; i++)
+        printf("%c", postfix[i]);
 
     getch();
     return 0;
---
+++ PostfixEvaluation.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#define MAX 50
+
+int main()
+{
+    int vstack[MAX];
+    int tos = -1;
+
+    char postfix[MAX];
+    int i = 0;
+
+    char ch;
+    printf("Enter the postfix expression:\n");
+    while (1)
+    {
+        ch = getch();
+        printf(" %c", ch);
+        if (ch == 13)
+            break;
+
+        postfix[i++] = ch;
+    }
+    postfix[i] = '\0';
+    printf("\n");
+
+    printf("\nvalue\top1\top2\tresult\tV-Stack\n");
+    printf("----------------------------------------\n");
+
+    int op1, op2, result;
+    for (i = 0; postfix[i] != '\0'; i++)
+    {
+        op1 = -1;
+        if (postfix[i] >= '0' && postfix[i] <= '9')
+            vstack[++tos] = postfix[i] - '0';
+        else
+        {
+            op1 = vstack[tos--];
+            op2 = vstack[tos--];
+            switch (postfix[i])
+            {
+            case '+':
+                result = op2 + op1;
+                break;
+            case '-':
+                result = op2 - op1;
+                break;
+            case '*':
+                result = op2 * op1;
+                break;
+            case '/':
+                result = op2 / op1;
+                break;
+            case '^':
+                result = pow(op2, op1);
+                break;
+            }
+            vstack[++tos] = result;
+        }
+
+        if (op1 != -1)
+            printf("%c\t%d\t%d\t%d\t", postfix[i], op1, op2, result);
+            else
+            printf("%c\t-\t-\t-\t", postfix[i]);
+
+        for (int j = 0; j <= tos; j++)
+        {
+            printf("%d ", vstack[j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nThe value is: %d", vstack[tos]);
+    getch();
+}
